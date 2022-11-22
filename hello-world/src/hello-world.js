c71b70dcd51caf3e5ca14f35fcb99c07e7d25679
MarcoTrc <marcoturchiuli@gmail.com>
2022-11-22 16:09:40 +0100

    test

--- hello-world/src/hello-world.js
+++ hello-world/src/hello-world.js
@@ -5,18 +5,12 @@ class Esponi {
 
     render() {
 
-    const heading = new Heading();
-    heading.render('hello world');
-    const helloWorldButton = new HelloWorldButton();
-    helloWorldButton.render();
-
-    if (process.env.NODE_ENV === 'production') {
-        console.log('Production mode');
-    } else if (process.env.NODE_ENV === 'development') {
-        console.log('Development mode');
+        const heading = new Heading();
+        heading.render('hello world');
+        const helloWorldButton = new HelloWorldButton();
+        helloWorldButton.render();
+        
     }
-}
-
 }
 
 export default Esponi;
